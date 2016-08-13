0fa631e162ca0620ef8084371e8ee122714c2c43
Ben <Ben@BABYGIRL-THE-SECOND>
2016-08-13 00:57:16 -0700

    deleted content

--- testGit/testGit/Source.cpp
+++ testGit/testGit/Source.cpp
@@ -8,31 +8,7 @@ int main()
 	cout << "you encounter an enemy on the road" << endl;
 	Enemy joe = { 10,13,5 };
 
-	cout << "\nHis name is joe and he is massive carrying a large great sword" << endl;
 	
-	cout << "Joe walks up to you\n" << endl;
-	
-	cout << "Enemy stats:" << endl;
-	cout << "	AC: " << joe.getAC() << endl;
-	cout << "	HP: " << joe.getHP() << endl;
-	cout << "	Cantrips: " << joe.getCant() << endl;
-
-	int dec = joe.chooseBehavior();
-	cout << "\nJoe decides to do option number " << dec << endl;
-
-	if (dec==1)
-	{
-		joe.attack();
-	}
-	else if (dec==2)
-	{
-		joe.speak();
-	}
-	else
-	{
-		joe.run();
-
-	}
 	
 	system("PAUSE");
 
